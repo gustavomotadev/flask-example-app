c6322a46a1ce13fc271671141ebeb76398f1b1f5
gustavomotadev <gustavomotadev@gmail.com>
2023-09-24 19:01:08 -0300

    Jinja templating and fake db

--- cardapio.py
+++ cardapio.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from http import HTTPStatus
+from json import load
+
+with open('banco.json', 'r', encoding='utf-8') as arquivo_json:
+    banco = load(arquivo_json)
 
 app = Flask(__name__)
+
+@app.errorhandler(HTTPStatus.NOT_FOUND)
+def page_not_found(erro):
+   return render_template('404.jinja'), HTTPStatus.NOT_FOUND
 
 @app.route('/')
 @app.route('/index')
 @app.route('/cardapio')
 def mostrar_cardapio():
-    return render_template('cardapio.html')
+    return render_template('cardapio.jinja', banco=banco)
 
-@app.route('/cardapio/pizza')
-def mostrar_pizza():
-    return render_template('pizza.html')
-
-@app.route('/cardapio/hamburguer')
-def mostrar_hamburguer():
-    return render_template('hamburguer.html')
+@app.route('/cardapio/<alimento>')
+def mostrar_alimento(alimento: str):
+    dados = banco.get(alimento)
+    if dados:
+        return render_template('alimento.jinja', 
+            nome=dados['nome'], 
+            descricao=dados['descricao'],
+            codigo=dados['codigo'],
+            url_imagem=dados['url_imagem'])
+    else:
+        abort(HTTPStatus.NOT_FOUND)
 
 if __name__ == '__main__':
     app.run(debug=True)
